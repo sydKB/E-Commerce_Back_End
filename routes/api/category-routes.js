54b9eafaaf41b617104bd8cfab0634ecdadbf5ed
sydKB <sydneykb11@icloud.com>
2023-06-28 05:20:25 -0700

    more route additions

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,30 +37,20 @@ router.get('/:id', async(req, res) => {
 router.post('/', async(req, res) => {
   // create a new category
   try {
-    const categoryData = await Category.create({
-      id: req.body.id,
-    });
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.put('/:id', async(req, res) => {
+router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  try {
-    const categoryData = await Category.send({
-        id: req.body.id,
-    });
-
-    if (!categoryData) {
-      res.status(404).json({ message: 'No category found with that id!' });
-      return;
-    }
-    res.status(200).json(categoryData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
 });
 
 router.delete('/:id', async(req, res) => {
