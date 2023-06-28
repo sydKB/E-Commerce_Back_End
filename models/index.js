45fb6e3f00dc55b8c64423feeb8bd322194a2d23
sydKB <sydneykb11@icloud.com>
2023-06-28 03:42:45 -0700

    route additions

--- models/index.js
+++ models/index.js
@@ -16,15 +16,13 @@ Category.hasMany(Product, {
 })
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+Product.belongsToMany(Tag, {
+  through: ProductTag,
 })
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+Tag.belongsToMany(Product, {
+  through: ProductTag,
 })
 
 module.exports = {
